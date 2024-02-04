78d6bb65a8347656659e8169e6bb1c229d7a99e7
alexngUNC <ageorgiev@unc.edu>
2024-02-04 13:16:40 -0500

    Fix vecAdd demo: remove child process

--- vecAdd.cu
+++ vecAdd.cu
@@ -1,5 +1,6 @@
+#include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <sys/wait.h>
 #include "testbench.h"
 #include "tmd.h"
 #include <unistd.h>
@@ -54,34 +55,33 @@ int main() {
     // Copy memory from host to device
     SAFE(cudaMemcpy(d_x, h_x, sizeof(int)*n, cudaMemcpyHostToDevice));
     SAFE(cudaMemcpy(d_y, h_y, sizeof(int)*n, cudaMemcpyHostToDevice));
+
+    // User will input their bit index
+    uint64_t userValue;
+    uint64_t fieldSize;
+
+    // Prompt the user for input
+    printf("Enter TMD field starting bit number: ");
+
+    // Read user input
+    if (scanf("%" SCNu64, &userValue) != 1) {
+        printf("Invalid input. Please enter a valid 64-bit unsigned integer.\n");
+        return 1;
+    }
+
+    printf("Enter TMD field size in bits: ");
     
-    // Print TMD struct field
-    pid_t child_pid;
-
-    // Fork the process
-    if ((child_pid = fork()) == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    if (child_pid == 0) {
-        // Execute the child TMD printing process
-        char* startArgs[2];
-        startArgs[0] = "start";
-        startArgs[1] = NULL;
-        if (execvp("./start", startArgs) == -1) {
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        }
-        exit(EXIT_SUCCESS);
-    } else {
-        // Wait for the TMD print process to complete
-        waitpid(child_pid, NULL, 0);
-    }
+    if (scanf("%" SCNu64, &fieldSize) != 1) {
+        printf("Invalid input. Please enter a valid 64-bit unsigned integer.\n");
+        return 1;
+    } 
 
     // Partition the L1 cache to allocate the desired amount of shared memory
     int carveout = (int) 100.0 * PERCENTAGE_SHARED;
     SAFE(cudaFuncSetAttribute(vecAdd, cudaFuncAttributePreferredSharedMemoryCarveout, carveout));
+
+    // Print TMD field
+    print_tmd_field(userValue, fieldSize);
 
     // Launch vector addition kernel
     vecAdd<<<8, 1024, n*sizeof(int)>>>(d_x, d_y, n);
